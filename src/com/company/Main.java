04dc723645560d4eded7d0fe30162b7e14c72bb6
Andrik2002 <andrikkobrin@gmail.com>
2022-03-17 20:46:59 +0200

    first try

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalAccessException {
+
+//      Перше завдання
+        Integer[] array = {22, 10, 20, 675, 99, 1002, 4323, 754};
+
+        /*Відсортування в порядку спадання*/
+        Arrays.sort(array, Collections.reverseOrder());
+        System.out.println("Відсортування в порядку спадання : " + Arrays.toString(array));
+
+        /*Відсортування в порядку зростання*/
+        Arrays.sort(array);
+        System.out.println("Відсортування в порядку зростання : " + Arrays.toString(array));
+
+//     Друге завдання
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Натисніть 1: щоб вивести toString()");
+            System.out.println("Натисніть 2: щоб засетити одинаковий  об`єкт класу Авто (fill()");
+            switch (scanner.next()) {
+                case "1": {
+                    Car[][] car = new Car[getRandomValue(1, 6)][getRandomValue(1, 6)];
+                    carDeepToString(car);
+                    System.out.println(Arrays.deepToString(car));
+                }
+                case "2": {
+                    Car[][] car = new Car[getRandomValue(1,6)][getRandomValue(1,6)];
+                    carDeepToString(car);
+                    System.out.println();
+
+                    Car carObject = new Car(getRandomValue(10, 100), getRandomValue(10, 100));
+
+                    for (Car[] a : car){
+                        Arrays.fill(a,carObject);
+                    }
+                    System.out.println(Arrays.deepToString(car));
+                }
+            }
+        }
+    }
+
+    public static void carDeepToString(Car[][] car) throws IllegalAccessException {
+        for (int i = 0; i < car.length; i++) {
+            for (int j = 0; j < car[i].length; j++) {
+                car[i][j] = new Car(getRandomValue(10, 100), getRandomValue(10, 100));
+            }
+        }
+    }
+
+    public static int getRandomValue(int min, int max) throws IllegalAccessException {
+        if (min >= max) {
+            throw new IllegalAccessException("Не правильне твердження");
+        }
+
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+}
